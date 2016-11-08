ebb587f6d0b627b48a61aa35811a46a89b0b3a18
franckinux <contact@franck-barbenoire.fr>
2016-11-08 22:05:39 +0100

    introduction gps

--- gps.py
+++ gps.py
@@ -1,11 +1,27 @@
 import asyncio
 from concurrent.futures import CancelledError
+from micropyGPS import MicropyGPS
+from serial_asyncio import create_serial_connection
+
+micro_gps = MicropyGPS()
 
 
-async def poll():
+class Output(asyncio.Protocol):
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def data_received(self, data):
+        for char in data:
+            micro_gps.update(chr(char))
+
+
+async def poll(loop):
+    coro = create_serial_connection(loop, Output, '/dev/ttyACM0', baudrate=115200)
+    asyncio.ensure_future(coro)
+
     while True:
-        # print("task2")
         try:
-            await asyncio.sleep(1)
+            await asyncio.sleep(3)
+            print(micro_gps.altitude, micro_gps.latitude, micro_gps.longitude)
         except CancelledError:
             break
--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from judebox import Judebox
 from gps import poll
 
 
-async def scheduler():
+async def scheduler(loop):
     judebox = Judebox()
     task1 = asyncio.ensure_future(judebox.judebox())
-    task2 = asyncio.ensure_future(poll())
+    task2 = asyncio.ensure_future(poll(loop))
     await task1
     task2.cancel()
     await task2
 
 gbulb.install()
 loop = asyncio.get_event_loop()
-loop.run_until_complete(scheduler())
+loop.run_until_complete(scheduler(loop))
 loop.close()
